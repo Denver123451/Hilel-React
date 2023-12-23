22e921a92d282a0ea9ee1f3b8f70e085cbf0c963
Yura Zhekov <yuriy.j@ezlogz.com>
2023-12-23 21:47:45 +0200

    Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
-import Card from "./Components/Card.jsx";
+import Counter from "./Components/Counter.jsx";
 
 function App() {
-  const cources = [
-    {
-      id: 1,
-      image:
-        "https://ultimatecourses.com/assets/share/courses/react-f02200115da09fd485a296e351972d7ea75701ed8d3d023d9f18c4b38e6b18b0.png",
-      level: "newbie",
-      title: "Introduction Basic Programming HTML & CSS",
-      user: {
-        avatar:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
-        name: "John Doe",
-      },
-      rating: 4.5,
-      students: 1234,
-      modules: 5,
-      duration: 5400,
-    },
-    {
-      id: 2,
-      image:
-        "https://ultimatecourses.com/assets/share/courses/react-f02200115da09fd485a296e351972d7ea75701ed8d3d023d9f18c4b38e6b18b0.png",
-      level: "Intermediate",
-      title: "React",
-      user: {
-        avatar:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png",
-        name: "John Doe",
-      },
-      rating: 4.7,
-      students: 3214,
-      modules: 8,
-      finishedModules: 3,
-      duration: 5400,
-      isMyCource: true,
-    },
-  ];
   return (
     <>
-      <h1>My curses</h1>
-      <Card items={cources} />
+      <h1>My counter</h1>
+      <Counter />
     </>
   );
 }
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,31 @@
+import "../styles.css";
+import { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handelIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handelDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleClear = () => {
+    setCounter(0);
+  };
+
+  return (
+    <>
+      <h2>{counter}</h2>
+
+      <div>
+        <button onClick={handelIncrement}>+</button>
+        <button onClick={handelDecrement}>-</button>
+        <button onClick={handleClear}>C</button>
+      </div>
+    </>
+  );
+};
+export default Counter;
